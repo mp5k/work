1f37b18576fe1f97e612bf1b6e9a6fb86864d369
mp5k <43346872+mp5k@users.noreply.github.com>
2018-09-19 07:14:09 +0900

    キー更新
    キー更新

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ class BaseConfig(object) :
     SECRET_KEY = os.urandom(24)
     DEBUG = False
     ASSISTANT_VERSION = '2018-07-13'
-    ASSISTANT_USERNAME = 'XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
-    ASSISTANT_PASSWORD = 'XXXXXXXXXXXX'
+    ASSISTANT_USERNAME = '58057b1d-6f4f-4e8f-ae6a-05a5eb7c8ba6'
+    ASSISTANT_PASSWORD = 'RmDgN2rq2QJO'
     PROTOCOL = ssl.PROTOCOL_TLS
     SERVER_CERT = ''
     SERVER_KEY =''
     CONFIDENCE = 0.19 #確信度の足切り
 
 class RuleConciergeConfig(BaseConfig) :
-    ASSISTANT_WORKSPACE = 'XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+    ASSISTANT_WORKSPACE = 'f22900fa-fe38-4a1c-bf09-0bc3e0e68e04'
 
 class DebugConfig(RuleConciergeConfig) :
     DEBUG = True
