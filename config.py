95ff6856f1aae2644c19cc99dc2846b008e63814
mp5k <43346872+mp5k@users.noreply.github.com>
2018-09-26 07:28:41 +0900

    chenge

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ class BaseConfig(object) :
     HOST = '0.0.0.0'
     SECRET_KEY = os.urandom(24)
     DEBUG = False
-    ASSISTANT_VERSION = '2018-07-13'
-    ASSISTANT_USERNAME = '58057b1d-6f4f-4e8f-ae6a-05a5eb7c8ba6'
-    ASSISTANT_PASSWORD = 'RmDgN2rq2QJO'
+    ASSISTANT_VERSION = '2018-08-30'
+    ASSISTANT_USERNAME = '0cb2ee89-74e8-4051-9edb-f27d163e9b38'
+    ASSISTANT_PASSWORD = 'mtigwya1idgC'
     PROTOCOL = ssl.PROTOCOL_TLS
     SERVER_CERT = ''
     SERVER_KEY =''
     CONFIDENCE = 0.19 #確信度の足切り
 
 class RuleConciergeConfig(BaseConfig) :
-    ASSISTANT_WORKSPACE = 'f22900fa-fe38-4a1c-bf09-0bc3e0e68e04'
+    ASSISTANT_WORKSPACE = '589ec8f3-a609-4260-b4e7-36c7f0b674f3'
 
 class DebugConfig(RuleConciergeConfig) :
     DEBUG = True
